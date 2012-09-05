f35151d4d58932ac0610203887f5f03f0e659f78
Michael Quandt <michael@mquandt.com>
2012-09-05 11:03:08 +1000

    Added Monogame, base framework in place.

---
+++ src/Samurai.Client.Win8/Samurai.Client.Win8/Program.cs
@@ -0,0 +1,14 @@
+﻿using MonoGame.Framework;
+using Windows.ApplicationModel.Core;
+
+namespace Samurai.Client.Win8
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            var factory = new GameFrameworkViewSource<SamuraiGame>();
+            CoreApplication.Run(factory);
+        }
+    }
+}
---
+++ src/Samurai.Client.Win8/Samurai.Client.Win8/SamuraiGame.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Samurai.Client.Win8
+{
+    public class SamuraiGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        public SamuraiGame()
+            : base()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
