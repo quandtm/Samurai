8818805d5cffdd1edc467d93cf3a825cf03cd0a0
Michael Quandt <michael@mquandt.com>
2012-09-05 13:36:50 +1000

    Progress!

---
+++ src/Samurai.Client.Win8/Samurai.Client.Win8/Graphics/Renderer.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Core;
+
+namespace Samurai.Client.Win8.Graphics
+{
+    public class Renderer
+    {
+        private CoreWindow _window;
+
+        public Renderer()
+        {
+        }
+
+        public bool Initialise(CoreWindow window)
+        {
+            return false;
+        }
+
+        public void Resize()
+        {
+            if (_window == null) return;
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
--- src/Samurai.Client.Win8/Samurai.Client.Win8/SamuraiGameView.cs
+++ src/Samurai.Client.Win8/Samurai.Client.Win8/SamuraiGameView.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Samurai.Client.Win8.Graphics;
 using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 
@@ -6,29 +6,35 @@ namespace Samurai.Client.Win8
 {
     public class SamuraiGameView : IFrameworkView
     {
+        private Renderer _renderer;
+        private bool _running;
+
         public void Initialize(CoreApplicationView applicationView)
         {
-            throw new NotImplementedException();
+            _renderer = new Renderer();
+            applicationView.CoreWindow.SizeChanged += (s, a) => _renderer.Resize();
+            _running = true;
         }
 
         public void Load(string entryPoint)
         {
-            throw new NotImplementedException();
         }
 
         public void Run()
         {
-            throw new NotImplementedException();
+            while (_running)
+            {
+                _renderer.Draw();
+            }
         }
 
         public void SetWindow(CoreWindow window)
         {
-            throw new NotImplementedException();
+            _renderer.Initialise(window);
         }
 
         public void Uninitialize()
         {
-            throw new NotImplementedException();
         }
     }
 }
